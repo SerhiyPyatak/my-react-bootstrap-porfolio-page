18df28f972f5d10700566affee21b83cd9137929
SerhiyPyatak <serhiypyatak@yahoo.com>
2021-07-15 11:31:51 +0300

    This commit adds particles animation background effect to hero section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Particles from 'react-particles-js';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from './components/Navbar';
@@ -6,6 +8,27 @@ import Hero from './components/Hero';
 function App() {
   return (
     <>
+      <Particles 
+        className = "particles-canvas"
+        params = {{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#f9ab00"
+              }
+            }
+          }
+        }}
+      />
       <Navbar />
       <Hero />
     </>
